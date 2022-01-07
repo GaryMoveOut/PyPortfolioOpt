9a5d5db68501eaa503e9422d18a2036c44d76625
GaryMoveOut <96015661+GaryMoveOut@users.noreply.github.com>
2022-01-07 16:50:52 +0100

    Exported TargetFunction type

--- pypfopt_fork/efficient_frontier/__init__.py
+++ pypfopt_fork/efficient_frontier/__init__.py
@@ -8,7 +8,8 @@ from .efficient_cvar import EfficientCVaR
 from .efficient_semivariance import EfficientSemivariance
 from .efficient_cdar import EfficientCDaR
 from .genetic_algorithm_utils import Constraints, Interval, SingleRoundParams, GeneticAlgorithmParams, \
-    force_portfolio_into_constraints, compute_portfolio_returns, compute_standard_deviation, compute_sharpe_ratio
+    force_portfolio_into_constraints, compute_portfolio_returns, compute_standard_deviation, compute_sharpe_ratio, \
+    TargetFunction
 
 __all__ = [
     "EfficientFrontier",
@@ -19,6 +20,7 @@ __all__ = [
     "Interval",
     "SingleRoundParams",
     "GeneticAlgorithmParams",
+    "TargetFunction",
     "compute_standard_deviation",
     "compute_portfolio_returns",
     "compute_sharpe_ratio",
